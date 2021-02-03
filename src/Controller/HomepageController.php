9cc9260434eb0df820d6cfc4126d9474b47b46a7
WellWedley <footwedley@gmail.com>
2021-02-03 09:42:01 +0100

    first push of wednesday

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\RechercheArticleFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +17,20 @@ class HomepageController extends AbstractController
     {
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="search_articles")
+     */
+    public function search(Article $article): Response
+    {
+        $article = new Article();
+        $searchForm = $this->createForm(RechercheArticleFormType::class, $article);
+
+        return $this->render('homepage/index.html.twig', [
+            'article' => $article,
+            'searchForm'=>$searchForm->createView()
         ]);
     }
 }
