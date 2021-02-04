e5099009154db2e61aa5fda4fdd9d5e85a02bc1a
Quentin Luzurier <q-luzurier@wanadoo.fr>
2021-02-04 09:40:33 +0100

    Avant mise en place connexion utilisateurs

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/connection", name="connection")
+     * @Route("/userlogin", name="user_login")
      */
-    public function connection(Request $request): Response
+    public function login(Request $request): Response
     {
         $email = $request->request->get('email');
         $password = $request->request->get('password');
-        return $this->render('account/connection.html.twig', [
+
+
+
+        return $this->render('account/user_login.html.twig', [
             "test" => $email,
         ]);
     }
@@ -24,21 +30,21 @@ class AccountController extends AbstractController
     /**
      * @Route("/newaccount", name="new_account")
      */
-    public function createAccount(Request $request): Response
+    public function createAccount(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setUsername($user->getEmail());
+            $user->setUsername($user->setRoles(["ROLE_USER"]));
+            $user->setPassword(password_hash($user->getPassword(), PASSWORD_DEFAULT));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('homepage');
+        }
         return $this->render('account/newaccount.html.twig', [
-            "test" => "creation compte",
+            "form_new_user" => $form->createView()
         ]);
     }
 }
-
-    /**
-     * @Route("/newaccount", name="new_account")
-     */
-    // public function index(): Response
-    // {
-    //     return $this->render('account/index.html.twig', [
-    //         'controller_name' => 'AccountController',
-    //     ]);
-    // }
-// }
