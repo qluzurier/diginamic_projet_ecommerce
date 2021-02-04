705a104571253bbeb431617e586b591535319766
Quentin Luzurier <q-luzurier@wanadoo.fr>
2021-02-04 19:59:55 +0100

    ajout suppression compte utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserFormType;
+use App\Form\UserFormModifyType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -28,7 +30,7 @@ class AccountController extends AbstractController
             $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirectToRoute('homepage');
+            return $this->redirectToRoute('app_login');
         }
         return $this->render('account/newaccount.html.twig', [
             "form_new_user" => $form->createView()
@@ -40,7 +42,7 @@ class AccountController extends AbstractController
      */
     public function showAccount(UserInterface $user, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(UserFormType::class, $user);
+        $form = $this->createForm(UserFormModifyType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($user);
@@ -48,7 +50,30 @@ class AccountController extends AbstractController
             return $this->redirectToRoute('homepage');
         }
         return $this->render('account/showaccount.html.twig', [
-            "form_user" => $form->createView()
+            "form_modify_user" => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/changepassword", name="password")
+     */
+    public function changePassword(UserInterface $user, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('account/password.html.twig', [
+            "test" => "tets"
+        ]);
+    }
+
+    /**
+     * @Route("/deleteaccount", name="delete_account")
+     */
+    public function deleteAccount(UserInterface $user, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($user);
+        $entityManager->flush();
+        $session = $this->get('session');
+        $session = new Session();
+        $session->invalidate();
+        return $this->redirectToRoute('homepage');
     }
 }
--- src/Form/UserFormModifyType.php
+++ src/Form/UserFormModifyType.php
@@ -3,40 +3,21 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserFormType extends AbstractType
+class UserFormModifyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'E-mail* :'
-            ])
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'first_options'  => [
-                    'label' => 'Mot de passe* :',
-                    'attr' => [
-                        'minlength' => 6,
-                        'placeholder' => "6 caractères minimum"
-                    ]
-                ],
-                'second_options' => [
-                    'label' => 'Confirmation du mot de passe* :',
-                    'attr' => [
-                        'minlength' => 6,
-                        'placeholder' => "6 caractères minimum"
-                    ]
-                ]
             ])
             ->add('nom', TextType::class, [
                 'label' => 'Nom* :',
@@ -75,7 +56,7 @@ class UserFormType extends AbstractType
                 ]
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Créer mon compte !'
+                'label' => 'Modifier mon compte'
             ]);
         ;
     }
