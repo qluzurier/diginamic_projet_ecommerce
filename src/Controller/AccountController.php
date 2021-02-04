d4bab8482795fb8657c9ecbb60fd9d65c71f1ce5
Quentin Luzurier <q-luzurier@wanadoo.fr>
2021-02-04 16:38:03 +0100

    Connexion des liens et routes

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AccountController extends AbstractController
 {
@@ -30,6 +31,23 @@ class AccountController extends AbstractController
         }
         return $this->render('account/newaccount.html.twig', [
             "form_new_user" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/account", name="account")
+     */
+    public function showAccount(UserInterface $user, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $entityManager->persist($user);
+        //     $entityManager->flush();
+        //     return $this->redirectToRoute('homepage');
+        // }
+        return $this->render('account/showaccount.html.twig', [
+            "form_user" => $form->createView()
         ]);
     }
 }
