17c556477b0cee84b3862fb123d3b934f096cccb
Kev <kevin.azuara.34110@gmail.com>
2021-02-07 15:33:05 +0100

    2eme tentative pull

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserFormType;
 use App\Form\UserFormModifyType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,35 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class AccountController extends AbstractController
 {
-    /**
-     * @Route("/newaccount", name="new_account")
-     */
-    public function createAccount(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserFormType::class, $user);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user->setUsername($user->getEmail());
-            $user->setUsername($user->setRoles(["ROLE_USER"]));
-            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash(
-                'success', 'Compte créé avec succès !'
-            );
-            return $this->redirectToRoute('app_login');
-        }
-        return $this->render('account/newaccount.html.twig', [
-            "form_new_user" => $form->createView()
-        ]);
-    }
-
     /**
      * @Route("/account", name="account")
      */
@@ -50,6 +24,9 @@ class AccountController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($user);
             $entityManager->flush();
+            $this->addFlash(
+                'success', 'Compte modifié avec succès !'
+            );
             return $this->redirectToRoute('homepage');
         }
         return $this->render('account/showaccount.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="Un compte utilisateur est déjà associé à cet email.")
  */
 class User implements UserInterface
 {
