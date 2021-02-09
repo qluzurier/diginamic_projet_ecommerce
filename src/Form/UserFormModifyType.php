4d491c4a1e6fa775daee0544522bd324174ca31c
Kev <kevin.azuara.34110@gmail.com>
2021-02-09 17:58:53 +0100

    Last pull ELISA + JNR + KEVIN

--- src/Form/UserFormModifyType.php
+++ src/Form/UserFormModifyType.php
@@ -17,20 +17,25 @@ class UserFormModifyType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'label' => 'E-mail* :'
+                'label' => 'E-mail* :',
+                'attr' => [
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;']
             ])
             ->add('nom', TextType::class, [
                 'label' => 'Nom* :',
                 'attr' => [
-                    'style' => 'text-transform: uppercase;'
-                ]
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;']
             ])
             ->add('prenom', TextType::class, [
-                'label' => 'Prénom* :'
+                'label' => 'Prénom* :',
+                'attr' => [
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;']
             ])
             ->add('adresse', TextType::class, [
                 'label' => 'Adresse :',
-                'required' => false
+                'required' => false,
+                'attr' => [
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;']
             ])
             ->add('code_postal', TextType::class, [
                 'label' => 'Code postal :',
@@ -38,25 +43,30 @@ class UserFormModifyType extends AbstractType
                 'attr' => [
                     'minlength' => "5",
                     'maxlength' => "5",
-                    'style' => 'text-transform: uppercase;'
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;'
                 ]
             ])
             ->add('ville', TextType::class, [
                 'label' => 'Ville :',
                 'required' => false,
                 'attr' => [
-                    'style' => 'text-transform: uppercase;'
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;'
                 ]
             ])
             ->add('tel', TelType::class, [
                 'label' => 'Téléphone :',
                 'required' => false,
                 'attr' => [
-                    'pattern' => '[0-9]{10}'
+                    'pattern' => '[0-9]{10}',
+                    'style' => 'text-transform: uppercase;width:30%; margin:15px;'
                 ]
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Modifier mon compte'
+                ,
+                'attr' => [
+                    'style' => ' margin-bottom:30px;'
+                ]
             ]);
         ;
     }
